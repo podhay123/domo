96365d7f870617bf8bf6063ff3454031439db21a
Podhay 123 <kuba.pod123@gmail.com>
2024-11-26 11:52:17 +0100

    p

---
+++ zadanie_domowe_19.11.2024/1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //przyjmujemy ze breaka maja wyrazenia o indeksach k1,k2,...
+    if (wyrazenie1){
+        dzialanie1
+        ...
+        dzialaniek1
+    }
+    if (wyrazenie2){
+        dzialanie2
+        ...
+        dzialaniek1
+    }
+    ...
+    if (wyrazeniek1){
+        dzialaniek1
+    }
+    if (wyrazeniek1+1){
+        dzialaniek1+1
+        dzialaniek1+2
+        ...
+        dzialaniek2
+    }
+    if (wyrazeniek1+2){
+        dzialaniek1+2
+        ...
+        dzialaniek2
+    }
+    if (wyrazeniek2){
+        dzialaniek2
+    }
+    ...
+}
