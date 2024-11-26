eb22347a18fd9b70624cc5f871ac5736f8f4c378
Podhay 123 <kuba.pod123@gmail.com>
2024-11-26 10:51:31 +0100

    19.11

---
+++ zadanie_domowe_19.11.2024/3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    przed1
+    for (inicjalizacja, koniec, inkrementacja){
+        przed2 
+        if (wyrazenie)
+            continue
+        po2
+    }
+    po1
+
+    ///
+
+    przed1
+    while(wyrazenie){
+        przed2
+        if (!wyrazenie)
+            po2
+    }
+    po1
+
+}
